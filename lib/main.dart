29ba74af857e80ccd380b80e5f00033556d0b0d1
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-05-23 23:26:06 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 import 'models/widget_model.dart';
+import 'provider/theme_provider.dart';
 import 'screens/home_screen.dart';
 import 'screens/widget_detail_screen.dart';
 import 'data/widget_data.dart';
 
 void main() {
-  runApp(const WidgetExplorerApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'Flutter Widget Explorer',
+      themeMode: themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepPurple,
+        scaffoldBackgroundColor: Colors.grey[50],
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.deepPurple,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+    );
+  }
 }
 
 final _router = GoRouter(
+  // initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
@@ -39,16 +72,3 @@ final _router = GoRouter(
     body: Center(child: Text('Page not found')),
   ),
 );
-
-class WidgetExplorerApp extends StatelessWidget {
-  const WidgetExplorerApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _router,
-      title: 'Flutter Widget Explorer',
-      theme: ThemeData(primarySwatch: Colors.blue),
-    );
-  }
-}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import '../components/widget_tile.dart';
+import 'package:provider/provider.dart';
 import '../data/widget_data.dart';
+import '../models/widget_model.dart';
+import '../provider/theme_provider.dart';
+
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,47 +14,172 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String _searchText = "";
+  String search = '';
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     final filteredWidgets = widgetList
-        .where((w) => w.name.toLowerCase().contains(_searchText.toLowerCase()))
+        .where((w) => w.name.toLowerCase().contains(search.toLowerCase()))
         .toList();
+
+    final isDarkMode = themeProvider.isDarkMode;
+    final baseBgColor = isDarkMode ? Colors.black : Colors.grey[50];
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Flutter Widget Explorer"),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple,
-        elevation: 4,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        toolbarHeight: 100,
+        title: ShaderMask(
+          shaderCallback: (bounds) => const LinearGradient(
+            colors: [Colors.deepPurple, Colors.pink],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ).createShader(bounds),
+          child: const Text(
+            'Flutter Widget Explorer',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        actions: [
+          Row(
+            children: [
+              Icon(isDarkMode ? Icons.nightlight_round : Icons.wb_sunny,
+                  color: Colors.white),
+              Switch(
+                value: isDarkMode,
+                onChanged: (_) => themeProvider.toggleTheme(),
+                activeColor: Colors.pinkAccent,
+                inactiveThumbColor: Colors.amber,
+              ),
+              const SizedBox(width: 12),
+            ],
+          )
+        ],
       ),
+      backgroundColor: baseBgColor,
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Search widgets...",
-                prefixIcon: const Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12.0),
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Center(
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 400),
+                child: TextField(
+                  onChanged: (val) => setState(() => search = val),
+                  decoration: InputDecoration(
+                    hintText: 'Search widgets...',
+                    prefixIcon: const Icon(Icons.search),
+                    filled: true,
+                    fillColor: isDarkMode ? Colors.grey[850] : Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
                 ),
               ),
-              onChanged: (value) {
-                setState(() => _searchText = value);
-              },
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: filteredWidgets.length,
-              itemBuilder: (context, index) {
-                return WidgetTile(widgetModel: filteredWidgets[index]);
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                final crossAxisCount = constraints.maxWidth > 800
+                    ? 4
+                    : constraints.maxWidth > 600
+                        ? 3
+                        : 2;
+
+                return GridView.builder(
+                  padding: const EdgeInsets.all(20),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: crossAxisCount,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    childAspectRatio: 1.2,
+                  ),
+                  itemCount: filteredWidgets.length,
+                  itemBuilder: (context, index) {
+                    final widgetItem = filteredWidgets[index];
+                    return _NeumorphicWidgetCard(
+                      widgetModel: widgetItem,
+                      onTap: () => context.go('/widget/${widgetItem.route}'),
+                      isDarkMode: isDarkMode,
+                    );
+                  },
+                );
               },
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _NeumorphicWidgetCard extends StatelessWidget {
+  final WidgetModel widgetModel;
+  final VoidCallback onTap;
+  final bool isDarkMode;
+
+  const _NeumorphicWidgetCard({
+    required this.widgetModel,
+    required this.onTap,
+    required this.isDarkMode,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final baseColor = isDarkMode ? Colors.grey[900]! : Colors.grey[300]!;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: baseColor,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: isDarkMode
+              ? [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.8),
+                    offset: const Offset(5, 5),
+                    blurRadius: 15,
+                  ),
+                  BoxShadow(
+                    color: Colors.grey.shade800,
+                    offset: const Offset(-5, -5),
+                    blurRadius: 15,
+                  ),
+                ]
+              : [
+                  BoxShadow(
+                    color: Colors.grey.shade500,
+                    offset: const Offset(5, 5),
+                    blurRadius: 15,
+                  ),
+                  const BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-5, -5),
+                    blurRadius: 15,
+                  ),
+                ],
+        ),
+        child: Center(
+          child: Text(
+            widgetModel.name,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: isDarkMode ? Colors.white70 : Colors.black87,
+              fontSize: 18,
+            ),
+          ),
+        ),
       ),
     );
   }
